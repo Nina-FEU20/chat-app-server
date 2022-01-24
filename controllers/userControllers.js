a149af60378cc9ea4893ae0adf871c5afb8a950e
Nina Jivetorp <ninajivetorp@gmail.com>
2022-01-24 03:34:49 +0100

    Add route to get all users and single user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -32,9 +32,30 @@ const loginUser = async(req, res) => {
     const correctPassword = await bcrypt.compare(password, user.password)
     if (!correctPassword) return res.status(400).send("Invalid username or password")
 
-    if(user) res.send({ id: user._id, username: user.username })
-    else res.send("Something went wrong")
+    if(user) res.status(200).json({ id: user._id, username: user.username })
+    else res.status(400).json({error: "Something went wrong"})
 
 }
 
-module.exports = { createUser, loginUser }
+const getAllUsers = async(req, res) => {
+    try{
+        const allUsers = await User.find({}, {username: 1, _id: 1})
+        res.status(200).json(allUsers)
+    } catch(err){
+        return res.status(400).json({ error: err })
+    }
+}
+
+const getSingleUser = async(req, res) => {
+
+    try{
+        const user = await User.find({ _id: req.params.id }, {username: 1, _id: 1})
+        res.status(200).json(user)
+    } catch(err){
+        return res.status(400).json({ error: err })
+    }
+}
+
+
+
+module.exports = { createUser, loginUser, getAllUsers, getSingleUser }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { createUser, loginUser } = require('../controllers/userControllers')
+const { createUser, loginUser, getAllUsers, getSingleUser } = require('../controllers/userControllers')
 
 const router = express.Router()
 
+router.get('/', getAllUsers)
+router.get('/:id', getSingleUser)
 router.post('/', createUser)
 router.post('/login', loginUser)
 
