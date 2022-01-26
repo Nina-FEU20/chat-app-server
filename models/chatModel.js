b20e7deb67faaa235560fa817f76ba07fc9a360c
Nina Jivetorp <ninajivetorp@gmail.com>
2022-01-26 09:28:39 +0100

    Add chat and messageModel

---
+++ models/chatModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }]
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model('Chat', chatModel);
+
+module.exports = Chat;
